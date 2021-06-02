fcc06ac69d5900271765631a94dd434bbd1d7aac
japananh <japananh@gmail.com>
2021-06-02 22:28:53 +0700

    Add action

---
+++ index.js
@@ -0,0 +1,32 @@
+const axios = require('axios')
+const fs = require('fs')
+
+const getQuote = async () => {
+  try {
+    const { data } = await axios.get('https://type.fit/api/quotes')
+    if (!data.length) return {}
+
+    const index = Math.floor(Math.random(data.length - 1))
+    const quote = data[index].text
+    const author = data[index].author
+    
+    return {
+      quote,
+      author
+    }
+  } catch(err) {
+    console.error(err.message)
+    return {}
+  }
+}
+
+const generate = async () => {
+  const { quote, author } = await getQuote()
+
+  if (!quote && !author) return
+  
+  fs.writeFileSync('README.md', `_**${quote}**_\n\n${author}`)
+}
+
+generate()
+
