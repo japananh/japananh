1ebeefeee52d14026b88e083966a844dc1d2bd2f
leminhson2398 <leminhson2398@outlook.com>
2021-09-16 22:49:22 +0700

    enhanced

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
-_**The greater part of human pain is unnecessary. It is self-created as long as the unobserved mind runs your life.**_
+_**I can't imagine a person becoming a success who doesn't give this game of life everything hes got.**_
 
-Eckhart Tolle
+>Walter Cronkite
+
+
+
+
+
+ p/s. Please think of me as smart working instead of hark working <img wisth="24" height="24" align="center" src="emoji/1f3fe.png" alt="emoji of the day">
+`2021/8/16`
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const axios = require('axios')
 const fs = require('fs')
 
+
+
 const getQuote = async () => {
   try {
     const { data } = await axios.get('https://type.fit/api/quotes')
@@ -9,24 +11,34 @@ const getQuote = async () => {
     const index = Math.floor(Math.random() * (data.length - 1))
     const quote = data[index].text
     const author = data[index].author
-    
+
     return {
       quote,
       author
     }
-  } catch(err) {
+  } catch (err) {
     console.error(err.message)
     return {}
   }
 }
 
+const getEmojiOfTheDay = async () => {
+  const data = fs.readFileSync("data.json");
+  const { emojis, _ } = JSON.parse(data.toString());
+
+  return emojis[Math.round(Math.random() * emojis.length)];
+}
+
 const generate = async () => {
   const { quote, author } = await getQuote()
+  const emoji = await getEmojiOfTheDay()
+
+  const now = new Date();
+  const formatNow = `\`${now.getFullYear()}/${now.getMonth()}/${now.getDate()}\``
 
   if (!quote && !author) return
-  
-  fs.writeFileSync('README.md', `_**${quote}**_\n\n${author}`)
+
+  fs.writeFileSync('README.md', `_**${quote}**_\n\n>${author}\n\n\n\n\n\n p/s. Please think of me as smart working instead of hark working <img wisth="24" height="24" align="center" src="emoji/${emoji}.png" alt="emoji of the day">\n ${formatNow}`)
 }
 
 generate()
-
